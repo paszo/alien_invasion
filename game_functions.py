774bf66d4eb87f60f944e7635169893236f67653
popo <paszo2008@gmail.com>
2019-04-01 21:20:20 +0200

    Added the left movement

--- game_functions.py
+++ game_functions.py
@@ -13,12 +13,14 @@ def check_events(ship):
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = True
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = True
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
-
-
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
