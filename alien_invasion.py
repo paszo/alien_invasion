ed5324091e85bd109a3e702a4410f12eda167cdb
popo <paszo2008@gmail.com>
2019-04-04 20:11:50 +0200

    Added alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 
@@ -15,15 +16,17 @@ def run_game():
 
     # Make a ship.
     ship = Ship(ai_settings, screen)
-    # Mate a group to store bullets in.
+    # Make a group to store bullets in.
     bullets = Group()
+    # Make an alien
+    alien = Alien(ai_settings, screen)
 
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 run_game()
