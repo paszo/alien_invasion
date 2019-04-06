534febbbd86f2e13b9042261b830ee972f7d226b
popo <paszo2008@gmail.com>
2019-04-06 21:12:56 +0200

    Added the score growing factor

--- settings.py
+++ settings.py
@@ -21,7 +21,9 @@ class Settings():
         self.fleet_drop_speed = 10
 
         # How quickly the game speeds up
-        self.speedup_scale = 1.5
+        self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -37,7 +39,10 @@ class Settings():
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
